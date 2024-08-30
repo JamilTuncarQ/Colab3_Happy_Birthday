54de37e3f04d753b9b29aaafda7f040523d4c0d4
JamilTuncarQ <jamil.tuncar@unmsm.edu.pe>
2024-08-29 23:28:59 -0500

    adicion de readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Happy Birthday
+
+![vista previa](Captura%20de%20pantalla%202024-08-29%20232452.png)
+![vista de ejecucion](Captura%20de%20pantalla%202024-08-29%20232538.png)
--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -30,17 +32,16 @@ import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             HappyBirthdayTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.onBackground
                 ) {
                     GreetingImage(
                         message = getString(R.string.happy_birthday_text),
                         from = getString(R.string.signature_text),
-                        //modifier = Modifier.padding(18.dp)
                     )
                 }
             }
